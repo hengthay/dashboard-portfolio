5e36b94cfff2b664c274f1db267cb172e9a6fa20
hengthay <laovkimhengthai@gmail.com>
2026-05-13 16:43:16 +0700

    Fix typo on SkillCreate.jsx

--- src/components/Skill/SkillCreate.jsx
+++ src/components/Skill/SkillCreate.jsx
@@ -155,7 +155,7 @@ const SkillCreate = () => {
                 <option value="Frotend Development">Frotend Development</option>
                 <option value="Backend Development">Backend Development</option>
                 <option value="Full-Stack Development">Full-Stack Development</option>
-                <option value="Databse Management">Database Management</option>
+                <option value="Database Management">Database Management</option>
                 <option value="DevOps & Tools">DevOps & Tools</option>
                 <option value="Mobile Development">Mobile Development</option>
                 <option value="Other">Other</option>
